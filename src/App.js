a38ab584156ed46ea83d9d63806830e7ece99dc7
Mohammed Elzanaty <mohammedelzanaty129@gmail.com>
2021-08-07 20:36:23 +0200

    replace suing audio instance by using useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import { useRef } from "react";
 import audioEl3asofra from "./el3asfor.mp3";
 import "./App.css";
 
 function App() {
-  const audio = new Audio(audioEl3asofra);
-  audio.loop = true;
+  const audioEl = useRef(null);
 
-  const handlePlayAudio = () => {
-    audio.play();
+ const handlePlayAudio = () => {
+    let current = audioEl.current;
+    // pause any audio before playing it again
+    if (!current.paused && !current.ended) {
+      current.pause();
+    }
+    // play audio again
+    current.play();
   };
 
   const handlePauseAudio = () => {
-    audio.pause();
+    audioEl.current.pause();
   };
 
   return (
     <div className="App">
       <header className="App-header">
         Redux Action El 3asfor Audio
-        <audio>
+        <audio ref={audioEl}>
           <source src={audioEl3asofra} type="audio/mp3" />
           Your browser does not support the audio element.
         </audio>
