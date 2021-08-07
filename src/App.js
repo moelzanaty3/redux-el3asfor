2cc2be9280e6e7277e1590d83a3427607d8672f3
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-08-07 20:13:44 +0200

    init project wihtout redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import audioEl3asofra from "./el3asfor.mp3";
+import "./App.css";
 
 function App() {
+  const audio = new Audio(audioEl3asofra);
+  audio.loop = true;
+
+  const handlePlayAudio = () => {
+    audio.play();
+  };
+
+  const handlePauseAudio = () => {
+    audio.pause();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Redux Action El 3asfor Audio
+        <audio>
+          <source src={audioEl3asofra} type="audio/mp3" />
+          Your browser does not support the audio element.
+        </audio>
+        <div className="App-actions">
+          <button className="primary" onClick={handlePlayAudio}>
+            PLAY
+          </button>
+          <button className="danger" onClick={handlePauseAudio}>
+            Pause
+          </button>
+        </div>
+        <main className="Counter">
+          <p className="count">0</p>
+        </main>
       </header>
     </div>
   );
